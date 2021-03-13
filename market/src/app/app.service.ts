574f24f3acabee1d768c592d55e12e51463e26fb
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-13 18:04:45 +0100

    Added Service to App with http call to /deals

---
+++ market/src/app/app.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { getDefaultSettings } from 'node:http2';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(public httpClient:HttpClient) {}
+
+  getDeals(): Observable<any> {
+    return this.httpClient.get("http://localhost:3000/deals");
+  }
+}
