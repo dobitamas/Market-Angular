b563d49666cac8cc120c5aecee04abb8999fc709
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-14 16:25:54 +0100

    Started to save selected theme in localstorage

--- market/src/app/nav/nav.component.ts
+++ market/src/app/nav/nav.component.ts
@@ -10,13 +10,25 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class NavComponent {
 
-  isDarkTheme: boolean = true;
+  isDarkTheme: boolean = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+
+    storeSelectedTheme() {
+      localStorage.setItem('theme', this.isDarkTheme? "Dark" : "Light")
+    }
+
+    ngOnInit(){
+      if (localStorage.getItem('theme') === "Dark") {
+        this.isDarkTheme = true;
+      } else {
+        this.isDarkTheme = false;
+      }
+    }
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
