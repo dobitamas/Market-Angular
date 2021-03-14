b4ae2a8442f3f9f2296e0075663794fb565b8b1c
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-14 16:46:21 +0100

    Added auto showing progressbar based on whether it's loading or not

--- market/src/app/loader/interceptor.service.ts
+++ market/src/app/loader/interceptor.service.ts
@@ -1,9 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { LoaderService } from './loader.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorService {
+export class InterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(public loaderService: LoaderService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loaderService.isLoadingNow.next(true);
+
+    return next.handle(req).pipe(
+      finalize(
+        () => {
+          this.loaderService.isLoadingNow.next(false);
+        }
+      )
+    )
+  }
 }
