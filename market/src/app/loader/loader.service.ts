4b8bc0d53094d59cb7cff8818f75d3fe89278f33
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-14 16:38:16 +0100

    Fixed autoredirection from unknown page

--- market/src/app/loader/loader.service.ts
+++ market/src/app/loader/loader.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
 
+  public isLoadingNow:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   constructor() { }
 }
