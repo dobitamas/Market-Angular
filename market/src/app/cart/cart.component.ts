8740ff8e31eb552e3f99f9b89fc66588a8488854
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-18 21:24:49 +0100

    Added notifications to the cart component

--- market/src/app/cart/cart.component.ts
+++ market/src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CartService } from '../cart.service';
+import { NotifierService } from '../notifier.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,22 +11,29 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items = [];
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+    private notifierService:NotifierService) { }
 
   
 
   ngOnInit(): void {
     this.cartService.getCart().subscribe(resp => {
-      console.log(resp)
       this.items = resp;
-      console.log(resp);
+      this.notifierService.showNotification("Successfully loaded cart!", "Okay.", "success");
+    },
+    error => {
+      this.notifierService.showNotification("Cart could not be loaded :(", "I got it!", "error");
     })
   }
 
 
   deleteFromCart(itemId: string) {
-    this.cartService.deleteFromCart(itemId).subscribe(data => {
+    this.cartService.deleteFromCart(itemId).subscribe(resp => {
       this.ngOnInit();
+      this.notifierService.showNotification("Item is deleted!", "I got it!", "success");
+    }, 
+    error => {
+      this.notifierService.showNotification("Item could not be deleted!", "Okay", "error");
     })
 
   }
